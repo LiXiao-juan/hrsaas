e18ee9796fbafbd461baef69a9a0f974ec9063ec
Li <1627843848@qq.com>
2022-08-05 11:32:13 +0800

    完成树形结构的显示

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,21 +1,47 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>组织架构</h2>
+      <el-card class="box-card">
+        <!-- 头部 -->
+        <Tree :treeNode="company" :isRoot="true"></Tree>
+        <!-- 树形 -->
+        <el-tree :data="departs" :props="defaultProps" default-expand-all>
+            <template v-slot="{ data }">
+              <Tree :treeNode="data" />
+            </template>
+        </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import Tree from '@/views/departments/components/tree-tools.vue'
 export default {
   data() {
-    return {}
+    return {
+      departs: [
+        { name: '总裁办', children: [{ name: '董事会' }] },
+        { name: '行政部' },
+        { name: '人事部' }
+      ],
+      defaultProps: {
+        label: 'name' // 展示到树状的数据
+      },
+      company: { name: '传智教育', manager: '负责人' }
+    }
   },
-
+  components: {
+    Tree
+  },
   created() {},
 
   methods: {}
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="scss">
+.el-dropdown-link {
+  cursor: pointer;
+}
+</style>
